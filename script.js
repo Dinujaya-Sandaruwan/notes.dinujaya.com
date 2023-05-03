656133eaf4f4e4470c4da35857fe858ddc97163c
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-05-03 06:06:49 +0530

    Finished JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const mobileMenuDisplayBlock = () => {
 	document.getElementById("mobileMenu").style.display = "block";
+	document.getElementById("cards").style.display = "none";
+	document.getElementById("footer").style.display = "none";
 };
 const mobileMenuDisplayNone = () => {
 	document.getElementById("mobileMenu").style.display = "none";
+	document.getElementById("cards").style.display = "flex";
+	document.getElementById("footer").style.display = "block";
 };
 
 function checkDeviceWidth() {
